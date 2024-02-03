31be22df8a87880e9260763d348a9703ff1d4260
Anikalogika4417 <anikalogika4417@gmail.com>
2024-02-03 18:49:41 +0200

    Dig_rec_mid

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib
-from sklearn import svm
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
-from sklearn import datasets
-from mlxtend.plotting import plot_decision_regions
+from sklearn import datasets, svm, metrics
 
+digits = datasets.load_digits()
 
+images_and_lables = list(zip(digits.images, digits.target))
+
+#for index, (image, lable) in enumerate(images_and_lables[:6]):
+    #plt.subplot(2,3, index+1)
+    #plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    #plt.title('Target: %i' % lable)
+
+#plt.show()
+
+data = digits.images.reshape((len(digits.images), -1))
