b95adaa193eb80306e1ea07ac6ff055071dd9af4
Anikalogika4417 <anikalogika4417@gmail.com>
2024-02-03 18:10:23 +0200

    Start1

--- main.py
+++ main.py
@@ -2,5 +2,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
 from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn import datasets
 from mlxtend.plotting import plot_decision_regions
+
+iris_data = datasets.load_iris()
+
+#print(iris_data.data.shape)
+#print(iris_data.target.shape)
+
+features = iris_data.data
+target = iris_data.target
+
+feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.3)
+
+model = svm.SVC()
+fitted_model = model.fit(feature_train, target_train)
+predictions = fitted_model.predict(feature_test)
+
+accuracy = accuracy_score(target_test, predictions)
+confusion_matrix = confusion_matrix(target_test, predictions)
+
+print(confusion_matrix)
+print(accuracy)
 
