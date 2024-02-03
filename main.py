23938bf85e814c7b1b1d66e4f1383833cdc22d16
Anikalogika4417 <anikalogika4417@gmail.com>
2024-02-03 18:14:10 +0200

    Param_tune_start

--- main.py
+++ main.py
@@ -19,12 +19,3 @@ target = iris_data.target
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.3)
 
 model = svm.SVC()
-fitted_model = model.fit(feature_train, target_train)
-predictions = fitted_model.predict(feature_test)
-
-accuracy = accuracy_score(target_test, predictions)
-confusion_matrix = confusion_matrix(target_test, predictions)
-
-print(confusion_matrix)
-print(accuracy)
-
